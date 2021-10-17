a7bc8edfb9a065fcfd556889501791004da812ab
BiancaCarneiro <bianca.ccnf@hotmail.com>
2021-10-17 16:50:34 -0300

    Recognize fingers

--- meme_rec.py
+++ meme_rec.py
@@ -1,23 +1,34 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 cam = cv2.VideoCapture(0)
 mphands = mp.solutions.hands
 hands = mphands.Hands()
 mpdraw =  mp.solutions.drawing_utils
+axisX = np.zeros(20)
+axisY = np.zeros(20)
 
 while 1:
     success, img = cam.read()
     if not success:
         print("Failed")
         break
-
+    height, width, c = img.shape
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handsl in results.multi_hand_landmarks:
             mpdraw.draw_landmarks(img, handsl, mphands.HAND_CONNECTIONS)
+            for id, lm in enumerate(handsl.landmark):
+                cx, cy = int(lm.x * width), int(lm.y * height)
+                print(id, cx, cy)
+                if id == 4 or id == 8:
+                    cv2.circle(img, (cx, cy), 15, (255,0,0), cv2.FILLED)
+                    axisX[id] = cx
+                    axisY[id] = cy
+                    
 
     cv2.imshow("Video", img)
     k = cv2.waitKey(1)
