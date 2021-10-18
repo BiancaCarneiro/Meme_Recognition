d2d7d0a8e4214871663e9f1e8bd31e5af8f73865
BiancaCarneiro <bianca.ccnf@hotmail.com>
2021-10-18 14:13:44 -0300

    Checks if the fingers are up

--- meme_rec.py
+++ meme_rec.py
@@ -1,17 +1,19 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+from math import pi
 
 cam = cv2.VideoCapture(0)
 mphands = mp.solutions.hands
 hands = mphands.Hands()
 mpdraw =  mp.solutions.drawing_utils
-axisX = np.zeros(20)
-axisY = np.zeros(20)
+axisX = np.zeros(21)
+axisY = np.zeros(21)
 fingers = np.array([[2,3,4],[5,6,8],[9,11,12],[13,15,16],[17,19,20]]) #polegar, indicador, dedo do meio, anelar e midinho
 
 def calculate_finger_angle(array):
     #At first, lets find the equations ax+by+c=0
+    #print(axisY[int(array[0])])
     a1 = axisY[array[0]] - axisY[array[1]]
     b1 = axisX[array[0]] - axisX[array[1]]
     m1 = -a1/b1
@@ -19,14 +21,18 @@ def calculate_finger_angle(array):
     b2 = axisX[array[1]] - axisX[array[2]]
     m2 = -a2/b2
     tgangle = abs((m1-m2)/(1+m1*m2))
+    #print(tgangle)
     angle = np.tan(tgangle)
+    angle = angle*(180)/(pi) 
+    print(angle)
     return angle
 
 def meme1():
-    if (axisX[4] <= axisX[8]+8 and axisX[4] >= axisX[8]-8) and axisX[4] != 0 and (axisY[4] <= axisY[8]+8 and axisY[4] >= axisY[8]-8) and axisY[4] != 0:               
-        meme1 = "Gallery/meme1.png"
-        img_meme1 = cv2.imread(meme1)
-        cv2.imshow("Meme", img_meme1)
+    if (axisX[4] <= axisX[8]+8 and axisX[4] >= axisX[8]-8) and axisX[4] != 0 and (axisY[4] <= axisY[8]+8 and axisY[4] >= axisY[8]-8) and axisY[4] != 0:
+        if calculate_finger_angle(fingers[2]) < 20 and calculate_finger_angle(fingers[3]) < 20 and calculate_finger_angle(fingers[4]) < 20:
+            meme1 = "Gallery/meme1.png"
+            img_meme1 = cv2.imread(meme1)
+            cv2.imshow("Meme", img_meme1)
 
 def main():
     while 1:
@@ -43,11 +49,8 @@ def main():
                 mpdraw.draw_landmarks(img, handsl, mphands.HAND_CONNECTIONS)
                 for id, lm in enumerate(handsl.landmark):
                     cx, cy = int(lm.x * width), int(lm.y * height)
-                    if id == 4 or id == 8:
-                        #cv2.circle(img, (cx, cy), 15, (255,0,0), cv2.FILLED)
-                        axisX[id] = cx
-                        axisY[id] = cy
-                        print(id, cx, cy)
+                    axisX[id] = cx
+                    axisY[id] = cy
             meme1()
         cv2.imshow("Video", img)
         k = cv2.waitKey(1)
