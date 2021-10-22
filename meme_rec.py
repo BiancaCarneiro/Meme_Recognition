f7e99e6088b83d3457159fb4528c5797cf341f84
BiancaCarneiro <bianca.ccnf@hotmail.com>
2021-10-22 11:07:14 -0300

    Fixed the third meme

--- meme_rec.py
+++ meme_rec.py
@@ -13,20 +13,20 @@ mp_face_mesh = mp.solutions.face_mesh
 face = mp_face_mesh.FaceMesh()
 axisX = np.zeros(21)
 axisY = np.zeros(21)
+axisZ = np.zeros(21)
 fingers = np.array([[2,3,4],[5,6,8],[9,11,12],[13,15,16],[17,19,20]]) #polegar, indicador, dedo do meio, anelar e midinho
+#fingers = np.array([[2,3,4],[5,6,8],[9,11,12],[13,15,16],[17,19,20]]) #polegar, indicador, dedo do meio, anelar e midinho
+
 
 def calculate_finger_angle(array, id):
     #At first, lets find the equations ax+by+c=0
     #print(axisY[int(array[0])])
-    a1 = abs(axisY[array[0]] - axisY[array[1]])
-    b1 = abs(axisX[array[0]] - axisX[array[1]])
-    m1 = -a1/b1
-    a2 = abs(axisY[array[1]] - axisY[array[2]])
-    b2 = abs(axisX[array[1]] - axisX[array[2]])
-    m2 = -a2/b2
-    tgangle = abs((m1-m2)/(1+m1*m2))
-    #print(tgangle)
-    angle = np.tan(tgangle)
+    vectorAB = np.array([axisX[array[0]] - axisX[array[1]], axisY[array[0]] - axisY[array[1]], axisZ[array[0]] - axisZ[array[1]]])
+    vectorBC = np.array([axisX[array[1]] - axisX[array[2]], axisY[array[1]] - axisY[array[2]], axisZ[array[1]] - axisZ[array[2]]])
+    ABdotBC = np.vdot(vectorAB, vectorBC)
+    normAB = np.linalg.norm(vectorAB)
+    normBC = np.linalg.norm(vectorBC)
+    angle = np.arccos(ABdotBC/(normAB*normBC))
     angle = abs(angle*(180)/(pi))
     #print(angle)
     if id == 3 or id == 4:
@@ -51,10 +51,6 @@ def meme2and3(): # billie and finger down
                 meme = "Gallery/meme2.png" 
             img_meme = cv2.imread(meme)
             cv2.imshow("Meme", img_meme)
-        elif axisY[8] > axisY[5] and axisY[12] > axisY[9]:
-            meme = "Gallery/meme3.png"
-            img_meme = cv2.imread(meme)
-            cv2.imshow("Meme", img_meme)
 
 
 def main():
@@ -68,15 +64,6 @@ def main():
         results_hands = hands.process(imgRGB)
         results_face = face.process(imgRGB)
         #print(results.multi_hand_landmarks)
-        if results_hands.multi_hand_landmarks:
-            for handsl in results_hands.multi_hand_landmarks:
-                mpdraw.draw_landmarks(img, handsl, mphands.HAND_CONNECTIONS, mp_drawing_styles.get_default_hand_landmarks_style(), mp_drawing_styles.get_default_hand_connections_style())
-                for id, lm in enumerate(handsl.landmark):
-                    cx, cy = int(lm.x * width), int(lm.y * height)
-                    axisX[id] = cx
-                    axisY[id] = cy
-            meme1()
-            meme2and3()
         if results_face.multi_face_landmarks:
             for face_landmarks in results_face.multi_face_landmarks:
                 mpdraw.draw_landmarks(
@@ -85,6 +72,17 @@ def main():
                     connections=mp_face_mesh.FACEMESH_CONTOURS,
                     landmark_drawing_spec=None,
                     connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
+        if results_hands.multi_hand_landmarks:
+            for handsl in results_hands.multi_hand_landmarks:
+                mpdraw.draw_landmarks(img, handsl, mphands.HAND_CONNECTIONS, mp_drawing_styles.get_default_hand_landmarks_style(), mp_drawing_styles.get_default_hand_connections_style())
+                for id, lm in enumerate(handsl.landmark):
+                    cx, cy, cz = int(lm.x * width), int(lm.y * height), int(lm.z)
+                    axisX[id] = cx
+                    axisY[id] = cy
+                    axisZ[id] = cz
+            meme1()
+            meme2and3()
+        
         cv2.imshow("Video", img)
         k = cv2.waitKey(1)
         if k%256 == 27: # Leaves with ESC
